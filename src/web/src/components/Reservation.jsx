5a917b1bae449e3e2c6ade61cd16d0b6ca7f72f2
Walber Gomes <walberg.s48@gmail.com>
2023-10-18 18:53:58 -0300

    :lipstick: att codigo Reservation e ReservationPlace components

--- src/web/src/components/Reservation.jsx
+++ src/web/src/components/Reservation.jsx
@@ -69,7 +69,7 @@ const Reservation = () => {
         </div>
       </div>
 
-      <div className="Map ">
+      <div className="Map overflow-hidden rounded-xl">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7291.592144185215!2d-43.96528176909546!3d-19.855770557430276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa691c5f83785cf%3A0x4d326dbb2c57b467!2sDal%20Grano%20Restaurante%20e%20Pizzaria!5e0!3m2!1spt-BR!2sbr!4v1696899378515!5m2!1spt-BR!2sbr"
           allowfullscreen=""
