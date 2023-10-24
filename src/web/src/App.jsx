5f426e7f9355492a91bc948c38852ed1a98f71a2
Walber Gomes <walberg.s48@gmail.com>
2023-10-24 18:51:42 -0300

    :sparkles: add Error Page

--- src/web/src/App.jsx
+++ src/web/src/App.jsx
@@ -1,13 +1,14 @@
 import Home from './pages/Home'
 import ProductList from './pages/ProductList'
 import Cart from './pages/Cart'
+import ErrorPage from './pages/ErrorPage'
 
 import './styles/global.css'
 
 function App() {
 
   return (
-    <Cart/>
+    <ErrorPage/>
   )
 }
 
