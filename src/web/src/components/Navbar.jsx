d16958e63d3ec60dd40465c475a44f2c0c13e18a
Walber Gomes <walberg.s48@gmail.com>
2023-10-24 19:22:32 -0300

    :construction: add router do home

--- src/web/src/components/Navbar.jsx
+++ src/web/src/components/Navbar.jsx
@@ -1,19 +1,23 @@
 import { Badge } from "@mui/material";
 import { Search, ShoppingBasketOutlined } from "@mui/icons-material";
 
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
   return (
     <div className="Container h-14">
       <div className="Wrapper py-2 px-5 flex justify-between items-center">
         <div className="Left flex-1 flex items-center">
-          <span className="Language text-sm cursor-pointer">BR</span>
+          {/* <span className="Language text-sm cursor-pointer">BR</span> */}
           <div className="SearchContainer border-2 border-gray-300 flex items-center ml-6 p-1">
             <input type="text" className="Input border-none outline-none" />
             <Search style={{color: "gray", fontSize: 16}}/>
           </div>
         </div>
         <div className="Center flex-1 text-center">
-          <h1 className="font-bold text-2xl text-orange-test"> PIZZARIA </h1>
+          <Link to={`/`}>
+            <h1 className="font-bold text-2xl text-orange-test cursor-pointer"> PIZZARIA </h1>
+          </Link>
         </div>
         <div className="Right flex-1">
           <div className="MenuItem text-sm cursor-pointer">
