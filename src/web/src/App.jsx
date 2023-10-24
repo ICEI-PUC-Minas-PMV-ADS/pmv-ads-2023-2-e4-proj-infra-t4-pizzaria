308889913ca61d66a83ffc1dda20a692e79aa984
Walber Gomes <walberg.s48@gmail.com>
2023-10-24 18:55:11 -0300

    :sparkles: git Product Page

--- src/web/src/App.jsx
+++ src/web/src/App.jsx
@@ -3,13 +3,14 @@ import ProductList from './pages/ProductList'
 import Cart from './pages/Cart'
 import ErrorPage from './pages/ErrorPage'
 import Login from './pages/Login'
+import Product from './pages/Product'
 
 import './styles/global.css'
 
 function App() {
 
   return (
-    <Login/>
+    <Product/>
   )
 }
 
