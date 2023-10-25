9d0d01eeaec108ac252ff31503adac72d5544a9e
Walber Gomes <walberg.s48@gmail.com>
2023-10-24 21:10:22 -0300

    :sparkles: link with API and route to categories

--- src/server/models/Product.js
+++ src/server/models/Product.js
@@ -1,16 +1,32 @@
+// const mongoose = require("mongoose");
+
+// const ProductsSchema = new mongoose.Schema(
+//   {
+//     title: { type: String, require: true, unique: true },
+//     desc: { type: String, require: true },
+//     img: { type: String, require: true },
+//     categories: { type: Array },
+//     size: { type: String },
+//     color: { type: String },
+//     price: { type: Number, require: true },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Products", ProductsSchema)
+
 const mongoose = require("mongoose");
 
-const ProductsSchema = new mongoose.Schema(
+const ProductSchema = new mongoose.Schema(
   {
-    title: { type: String, require: true, unique: true },
-    desc: { type: String, require: true },
-    img: { type: String, require: true },
-    categories: { type: Array },
-    size: { type: String },
-    color: { type: String },
-    price: { type: Number, require: true },
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    categories: { type: String },
+    price: { type: Number, required: true },
+    inStock: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Products", ProductsSchema)
+module.exports = mongoose.model("Product", ProductSchema);
--- src/web/src/components/Products.jsx
+++ src/web/src/components/Products.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react"
 import { popularProducts } from "../data"
 import Product from "./Product"
 
+import axios from "axios"
+
 const Products = ({cat, sort}) => {
-  console.log(cat, sort)
+  //console.log(cat, sort)
+
+  const [products, setProducts] = useState([])
+  //const [filteredProducts,SetfilteredProducts] = useState([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get( cat ? `http://localhost:5000/api/products?category${cat}`: `http://localhost:5000/api/products`)
+        
+        //console.log(res)
+
+        setProducts(res.data)
+
+        console.log(res.data)
+      } catch (error) {}
+    }
+    getProducts()
+  }, [cat])
+
   return (
     <div className="Container p-5 grid grid-cols-4">
-      {popularProducts.map(item => (
+      {products.map(item => (
         <Product item={item} key={item.id} />
       ))}
     </div>
