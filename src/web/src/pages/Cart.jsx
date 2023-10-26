3e010567042e65e466f9ac1ef3e060eb739fc3d1
Walber Gomes <walberg.s48@gmail.com>
2023-10-26 16:10:21 -0300

    :heavy_plus_sign: add Stripe and fix Redux dependence

--- src/web/src/pages/Cart.jsx
+++ src/web/src/pages/Cart.jsx
@@ -1,11 +1,25 @@
-import Navbar from "../components/Navbar"
-import Announcement from "../components/Announcement"
-import Footer from "../components/Footer"
-import { Add, Remove } from "@mui/icons-material"
-import { useSelector } from "react-redux"
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+
+import { Add, Remove } from "@mui/icons-material";
+
+import { useSelector } from "react-redux";
+
+import StripeCheckout from "react-stripe-checkout";
+import { useState } from "react";
+
+const KEY = process.env.REACT_APP_STRIPE;
 
 const Cart = () => {
-  const cart = useSelector((state) => state.cart)
+  const cart = useSelector((state) => state.cart);
+  const [stripeToken, setStripeToken] = useState(null)
+
+  const onToken = (token) => {
+    setStripeToken(token)
+  }
+  console.log(stripeToken)
+
   return (
     <div className="Container ">
       <Navbar />
@@ -13,12 +27,20 @@ const Cart = () => {
       <div className="Wrapper p-5">
         <h1 className="font-light text-4xl text-center ">Pedidos</h1>
         <div className="Top flex items-center justify-between p-5">
-          <button className="p-2 font-semibold cursor-pointer border-2 border-black">CONTINUE COMPRANDO</button>
+          <button className="p-2 font-semibold cursor-pointer border-2 border-black">
+            CONTINUE COMPRANDO
+          </button>
           <div className="TopTexts ">
-            <span className="underline cursor-pointer my-0 mx-2">Carrinho (2)</span>
-            <span className="underline cursor-pointer my-0 mx-2">Lista de Desejos (0)</span>
+            <span className="underline cursor-pointer my-0 mx-2">
+              Carrinho (2)
+            </span>
+            <span className="underline cursor-pointer my-0 mx-2">
+              Lista de Desejos (0)
+            </span>
           </div>
-          <button className="p-2 font-semibold cursor-pointer border-2 border-black bg-black text-white">CHECKOUT</button>
+          <button className="p-2 font-semibold cursor-pointer border-2 border-black bg-black text-white">
+            CHECKOUT
+          </button>
         </div>
 
         <div className="Bottom flex justify-between">
@@ -28,23 +50,30 @@ const Cart = () => {
                 <div className="ProductDetail flex-2 flex">
                   <img className="w-40" src={product.img} />
                   <div className="Datails p-5 flex flex-col justify-around">
-                    <span><b>Produto:</b> {product.title}</span>
-                    <span><b>ID:</b> {product._id} </span>
+                    <span>
+                      <b>Produto:</b> {product.title}
+                    </span>
+                    <span>
+                      <b>ID:</b> {product._id}{" "}
+                    </span>
                   </div>
                 </div>
                 <div className="PriceDetail flex-one flex flex-col items-center justify-center">
                   <div className="ProductAmountContainer flex items-center mb-5">
                     <Add />
-                    <div className="ProductAmount text-2xl m-2">{product.quantity}</div>
+                    <div className="ProductAmount text-2xl m-2">
+                      {product.quantity}
+                    </div>
                     <Remove />
                   </div>
-                  <div className="ProductPrice text-3xl font-extralight">R$ {product.price * product.quantity}</div>
+                  <div className="ProductPrice text-3xl font-extralight">
+                    R$ {product.price * product.quantity}
+                  </div>
                 </div>
               </div>
             ))}
 
             <hr className="bg-gray-200 border-none h-1" />
-
           </div>
           <div className="Summary flex-one border-2 border-gray-300 rounded-xl p-5 h-70vh">
             <h1 className="font-extralight text-3xl">RESUMO DO PEDIDO</h1>
@@ -65,14 +94,26 @@ const Cart = () => {
               <span className="">Total:</span>
               <span>R$ {cart.total}</span>
             </div>
-            <button className="w-full p-3 bg-black text-white font-semibold">CHECKOUT</button>
+            <StripeCheckout
+              name="Pizzaria"
+              image="https://avatars.githubusercontent.com/u/1486366?v=4"
+              billingAddress
+              shippingAddress
+              description={`O total do seu pedido é R$ ${cart.total}`}
+              amount={cart.total*100}
+              token={onToken}
+              stripeKey={KEY}
+            >
+              <button className="w-full p-3 bg-black text-white font-semibold">
+                CHECKOUT
+              </button>
+            </StripeCheckout>
           </div>
         </div>
       </div>
       <Footer />
-
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- src/web/src/requestMethods.js
+++ src/web/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/"
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MWNhMWJiMWIwMzZhY2U0NWNiYWIxNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5ODI2NTIzOCwiZXhwIjoxNjk4NTI0NDM4fQ.aC3yM1IyKo2c1TUeqFcrGb8vGosuaVwY2boI2LVjBik"
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MWNhMWJiMWIwMzZhY2U0NWNiYWIxNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5ODM0NTgxMCwiZXhwIjoxNjk4NjA1MDEwfQ.qXOVWAkfGA5fMKn03oSdDojuwa2FWeB458dODRjNciA"
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
